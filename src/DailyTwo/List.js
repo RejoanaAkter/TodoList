a2906d97f0086d12ffda06b7f150ea5ed7066f99
rejoana akter <razowanamimi@gmail.com>
2023-09-09 11:44:31 +0600

    first commit

--- src/DailyTwo/List.js
+++ src/DailyTwo/List.js
@@ -1,58 +1,81 @@
-import React, { useState, useEffect } from 'react'
-import './daily.css'
+import React, { useState, useEffect } from "react";
+import "./daily.css";
 
-import { GoChecklist} from 'react-icons/go'
+import { GoChecklist } from "react-icons/go";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
 const List = () => {
- const [input, setInput]= useState("")
- const [array, setArray]= useState([])
+  const [input, setInput] = useState("");
+  const [array, setArray] = useState([]);
 
+  useEffect(() => {
+    const savedTasks = JSON.parse(localStorage.getItem("array")) || [];
+    setArray(savedTasks);
+  }, []);
 
+  useEffect(() => {
+    localStorage.setItem("array", JSON.stringify(array));
+  }, [array]);
 
- useEffect(() => {
-  const savedTasks = JSON.parse(localStorage.getItem("array")) || [];
-  setArray(savedTasks);
-}, []);
+  const handleChange = (e) => {
+    setInput(e.target.value);
+  };
 
-useEffect(() => {
-  localStorage.setItem("array", JSON.stringify(array));
-}, [array]);
+  const handleClick = () => {
+if(input){
+  setArray([...array, input]);
 
-
- const handleChange=(e)=>{
-  setInput(e.target.value)
- }
-
-const handleClick=()=>{
-  setArray([...array, input])
-  setInput("")
 }
-
-const handleDelete=(index)=>{
-const todos=array.filter(( arr , ind)=>ind !== index);
-setArray(todos)
-  
+else{
+  alert('Add your todos first!')
 }
+ 
+    setInput("");
+  };
+
+  const handleDelete = (index) => {
+    const todos = array.filter((arr, ind) => ind !== index);
+    setArray(todos);
+  };
+
+  return (
+    <>
+      <div className="cover">
+        <div className="black-shadow-cover">
+          <div className="todo-app">
+            <h1 className="todo-header">
+              Todo-App
+              <GoChecklist
+                style={{
+                  color: "cadetblue",
+                  marginLeft: "2%",
+                  fontSize: "25px",
+                }}
+              />{" "}
+            </h1>
+            <h3
+              style={{
+                fontFamily: "great Vibes",
+                color: "lightblue",
+                textAlign: "center",
+              }}
+            >
+              Mimi
+            </h3>
+            <div className="todo-form">
+              <input  
+                placeholder="Add your todos.."
+                className="data-input"
+                value={input}
+                onChange={handleChange}
+              />
+              <button className="add-btn" onClick={handleClick}>
+                Add
+              </button>
 
 
 
- return(
-<>
-
-<div className="cover">
-    <div className="black-shadow-cover">
-    <div  className="todo-app">
-  <h1  className="todo-header">Todo-App<GoChecklist style={{color:"cadetblue",marginLeft:"2%", fontSize:"25px"}}/> </h1>
-  <h3  style={{fontFamily:"great Vibes",color:"lightblue", textAlign:"center"}}>Mimi</h3>
-  <div className="todo-form">
-<input placeholder='Add your todos..'  className='data-input'   value={input} onChange={handleChange} />
-<button className='add-btn' onClick={handleClick}>add</button>
-
-
-
-
-<div className="fulldata">
+              <div className="fulldata">
 {array.map((item, index)=>(
   <div className="single-todo"  key={index}>
  
@@ -71,11 +94,8 @@ setArray(todos)
     </div>
     </div>
       </div>
-
-</>
-
- )
-
+    </>
+  );
 };
 
-export default List
+export default List;
